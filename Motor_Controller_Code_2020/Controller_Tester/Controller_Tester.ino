4eb095d06e0479e911a60de340678b9cff94bedd
camhan22 <hansoncameron18@yahoo.com>
2021-07-05 14:19:42 -0400

    Update Controller_Tester.ino

--- Motor_Controller_Code_2020/Controller_Tester/Controller_Tester.ino
+++ Motor_Controller_Code_2020/Controller_Tester/Controller_Tester.ino
@@ -1,30 +1,39 @@
 #include <Wire.h>
 
+byte value = 0;
+bool dir = 0;
+int throttle_val = 0;
+int throttle_status = 16;
+
+
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
   Serial.begin(9600);
-  
+
   pinMode(5, INPUT); // Pin used to reset the controller
   pinMode(6, OUTPUT); // Used to reset the controller
   pinMode(A0, INPUT); //Used to set the speed
   pinMode(7, INPUT_PULLUP); //Used to set the direction
 
-// Start the motor controller
+  // Start the motor controller
   digitalWrite(6, LOW);
   delay(300);
   digitalWrite(6, HIGH);
 }
 
-byte value = 0;
-bool dir = 0;
-byte duty = 0;
+
 
 
 void loop() {
 
-  duty = map(analogRead(A0), 0, 1023, 0, 100); //Read the value and map it between 0 and 100.
+  Wire.requestFrom(8, 2); // Request the controller status from the motor controller
+  throttle_val = Wire.read();
+  throttle_status = Wire.read();
+  Serial.print(throttle_status);
+  Serial.print("-");
+
   dir = digitalRead(7);
-  value = bitWrite(duty, 7, dir);
+  value = bitWrite(throttle_val, 7, dir);
 
   Wire.beginTransmission(7); // transmit to device #8
   Wire.write(value);              // sends one byte
